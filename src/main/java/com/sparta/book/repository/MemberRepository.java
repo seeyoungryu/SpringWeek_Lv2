90342bd4d8601ad0465288b978a8d24ae25e305e
seeyoungryu <seeyoungryu@gmail.com>
2023-11-09 16:40:25 +0900

    오류해결전

--- src/main/java/com/sparta/book/repository/MemberRepository.java
+++ src/main/java/com/sparta/book/repository/MemberRepository.java
@@ -4,6 +4,4 @@ import com.sparta.book.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
-    Object save(Member member);
 }
